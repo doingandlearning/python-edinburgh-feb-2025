78c3bdd220406f7f8a58a4007b11e206c29921f5
Kevin Cunningham <github@kevincunningham.co.uk>
2025-02-25 12:03:48 +0000

    fixes bug in solution

--- 06-files/lab/solution/solution.py
+++ 06-files/lab/solution/solution.py
@@ -5,29 +5,33 @@ print(datetime.now())
 # datetime.datetime.now()
 
 with open("server.log", "w", newline="\n") as file:
-    file.write("""2024-12-04 10:23:45,INFO,Service started
+    file.write(
+        """2024-12-04 10:23:45,INFO,Service started
 2024-12-04 10:24:05,WARNING,High memory usage
 2024-12-04 10:25:12,ERROR,Service failed
 2024-12-04 10:26:30,INFO,Service restarted
-""")
+"""
+    )
 
 with open("server.log", "a", newline="\n") as file:
-    file.write(f"{datetime.now().strftime("%Y-%m-%d %H:%M:%S")},INFO,Logged from function")
+    file.write(
+        f"{datetime.now().strftime("%Y-%m-%d %H:%M:%S")},INFO,Logged from function"
+    )
 
 log_entries = []
 with open("server.log") as log_file:
     for line in log_file:
-        timestamp, severity, message = line.strip().split(",")
-        log_entries.append({
-            "timestamp": timestamp,
-            "severity": severity,
-            "message": message
-        })
+        result = line.strip().split(",")
+        log_entries.append(
+            {"timestamp": result[0], "severity": result[1], "message": result[2]}
+        )
 
 print(log_entries)
 
+
 def filter_logs_by_severity(log_entries, severity):
     return [entry for entry in log_entries if entry["severity"] == severity]
+
 
 def filter_logs_by_severity(log_entries, severity):
     result = []
@@ -36,11 +40,14 @@ def filter_logs_by_severity(log_entries, severity):
             result.append(entry)
     return result
 
+
 for severity in ["INFO", "WARNING", "ERROR"]:
     filtered_log = filter_logs_by_severity(log_entries, severity)
     with open(f"{severity.lower()}_logs.txt", "w", newline="\n") as severity_file:
         for entry in filtered_log:
-            severity_file.write(f'{entry["timestamp"]},{entry["severity"]},{entry["message"]}\n')
+            severity_file.write(
+                f'{entry["timestamp"]},{entry["severity"]},{entry["message"]}\n'
+            )
 
 print("Printed filtered logs.")
 
